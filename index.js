dac7746dd6a29b3112872bf4ca8f1e007892bae9
Sean Kramer <seandominickramer@gmail.com>
2020-03-25 10:05:37 -0400

    Add Person constructor

--- index.js
+++ index.js
@@ -39,8 +39,11 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name,age) {
+    this.stomach = [];
+}
+Person.prototype.eat = ("some food") => {
+  
 }
 
 /*
