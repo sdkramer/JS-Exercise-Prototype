b284ae80fec330ed31317a171c70532097efeb26
Sean Kramer <seandominickramer@gmail.com>
2020-03-25 17:03:05 -0400

    Add arguments and play function to Baby constructor

--- index.js
+++ index.js
@@ -88,14 +88,15 @@ Car.prototype.fill = function(gallons) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby(babyAttributes) {
-  Person.call(this, babyAttributes);
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
 
 Baby.prototype = Object.create(Person.prototype);
 
 Baby.prototype.play = function(favoriteToy){
-  return `Playing with ${favoriteToy}`;
+  return `Playing with ${this.favoriteToy}`;
 }
 /* 
   TASK 4
